2b028e3127810788c85b039e98da3d8eb206b6a8
Tomperez98 <tomasperezalvarez@gmail.com>
2024-06-27 12:51:43 -0500

    Add tests cases to fio

--- pkg/q/q_test.go
+++ pkg/q/q_test.go
@@ -1,6 +1,10 @@
 package q
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestQueue(t *testing.T) {
 	q := Queue[int]{}
@@ -9,15 +13,10 @@ func TestQueue(t *testing.T) {
 	q.Enqueue(3)
 
 	qe, _ := q.Dequeue()
-	if qe != 1 {
-		t.Errorf("expected 1, got %d", qe)
-	}
-
+	assert.Equal(t, qe, 1, "Value should be 1")
 	expected := 2
 	for qe := range q.Pop() {
-		if qe != expected {
-			t.Errorf("expected %d, got %d", expected, qe)
-		}
+		assert.Equal(t, qe, expected, "expected %d, got %d", expected, qe)
 		expected++
 	}
 }
